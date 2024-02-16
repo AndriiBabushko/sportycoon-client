17a90bf1a5f40b02f480bf9a381f7814af32bc1e
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-02-16 22:49:29 +0200

    try to fix deployment #8

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +14,12 @@ interface Props {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: Props) {
+const RootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
