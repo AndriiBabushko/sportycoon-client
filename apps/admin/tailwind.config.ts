8799c383bd81de9d94b7d85ea870c962248235dd
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-04-20 14:59:33 +0300

    resolve dev merge conflict

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -1,16 +1,1 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+export * from "@sportycoon/ui/tailwind.config";
--- apps/main/app/layout.server.tsx
+++ apps/main/app/layout.server.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@sportycoon/ui/globals.css";
 import { FC, ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
