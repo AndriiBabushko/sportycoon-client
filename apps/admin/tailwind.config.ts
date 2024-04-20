72c8f3f1a146a3bb03c538be715ab0415384d57f
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-04-20 17:20:23 +0300

    change proj to working with shadsn and storybook (wasted 3 hours to debug)

--- apps/admin/tailwind.config.ts
+++
@@ -1,1 +0,0 @@
-export * from "@sportycoon/ui/tailwind.config";
--- apps/main/app/page.server.tsx
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { FC } from "react";
-
-const Home: FC = () => {
-  return <h1>MAIN</h1>;
-};
-
-export default Home;
