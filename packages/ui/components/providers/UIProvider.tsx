17ecaa096c5dbec9dff11e8686a7e36686d96385
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-05-06 17:53:40 +0300

    try to fix no theme on vercel

--- packages/ui/components/providers/UIProvider.tsx
+++ packages/ui/components/providers/UIProvider.tsx
@@ -2,21 +2,18 @@
 
 import * as React from "react";
 import { ReactNode } from "react";
-import { COLORS } from "@/lib/colors";
 import { Theme } from "@radix-ui/themes";
-//
-// const theme = extendTheme({
-//   colors: {
-//     sportycoon: {
-//       100: COLORS.WHITE_GRAY,
-//       300: COLORS.LIGHT_GRAY,
-//       500: COLORS.GRAY,
-//       600: COLORS.MEDIUM_GRAY,
-//       700: COLORS.DARK_GRAY,
-//     },
-//   },+
-// });
+import { ThemeProvider } from "next-themes";
 
 export function UIProvider({ children }: { children: ReactNode }) {
-  return <Theme appearance={"dark"}>{children}</Theme>;
+  return (
+    <ThemeProvider
+      attribute={"class"}
+      defaultTheme={"light"}
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Theme appearance={"dark"}>{children}</Theme>
+    </ThemeProvider>
+  );
 }
