ddd3ce124296332b1fa8f3aa0bcffdc8702a22f5
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-05-06 17:20:17 +0300

    complete privacy policy/terms and conditions

---
+++ packages/ui/lib/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  WHITE_GRAY: "#DDDDDC", // 100
+  LIGHT_GRAY: "#BBBBBA", // 300
+  GRAY: "#989897", // 500
+  MEDIUM_GRAY: "#767675", // 600
+  DARK_GRAY: "#545452", // 700
+  DARKEST_GRAY: "#2d2d2c", // 800
+  DARK: "#1c1b1b", // 900
+} as const;
