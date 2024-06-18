fcadd923847fddeb6d25f2595b5d59d2a7061f59
Andrii Babushko <andriibabushko@gmail.com>
2024-06-18 20:41:00 +0300

    Fix environment variables for projects

--- packages/api/auth/spotify.ts
+++ packages/api/auth/spotify.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { axiosInstance } from "@/utils/axios";
+import { axiosInstance } from "@/utils";
 
 export const useSpotifyAuth = () => {
   return useQuery<{}, {}, {}>({
--- packages/api/provider.tsx
+++ packages/api/provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ApolloProvider } from "@apollo/client";
 import React, { FC } from "react";
 import { client } from "./utils";
