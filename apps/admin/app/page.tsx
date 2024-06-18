2b13694bdf006a7e21e86dabe38abd09382facee
Andrii Babushko <andriibabushko@gmail.com>
2024-06-18 20:41:00 +0300

    Fix environment variables for projects

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "ui";
 
 export default function Page() {
+  console.log("process.env.SPORTYCOON_API_URL", process.env.SPORTYCOON_API_URL);
+  console.log("process.env.CI", process.env.CI);
+
   return (
     <div className="container flex flex-col gap-5">
       <h1 className="text-red-700">Docs</h1>
       <div>
-        <Button rounded={"full"}>Hello</Button>
+        <Button rounded={"full"}>dfdfdfdf</Button>
       </div>
     </div>
   );
