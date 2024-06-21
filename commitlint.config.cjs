865162aafcb9d60b59e069d5007c19a35976f1b5
Andrii Babushko <andriibabushko@gmail.com>
2024-06-21 18:33:55 +0300

    fix: correct linter issue №20

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,24 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     'header-min-length': [2, 'always', 20],
-    'header-case-start-capital': [2, 'always'],
-    'header-end-period': [0, 'always'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
   },
-  plugins: [
-    {
-      rules: {
-        "header-case-start-capital": ({ raw }) => {
-          return [
-            /^[a-z]/.test(raw),
-            "Commit message must start with a lower case letter",
-          ];
-        },
-        "header-end-period": ({ header }) => {
-          return [/\.$/.test(header), "Commit message must end with a period"];
-        },
-      },
-    },
-  ],
 };
