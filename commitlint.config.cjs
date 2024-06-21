f9262e16bcd42817edf7f91e423d11723d1a4b6b
Andrii Babushko <andriibabushko@gmail.com>
2024-06-21 15:47:16 +0300

    Fix: correct linter issue №15.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "header-min-length": [2, "always", 10],
-    "header-case-start-capital": [2, "always"],
-    "header-end-period": [2, "always"],
+    'header-min-length': [2, 'always', 20],
+    'header-case-start-capital': [2, 'always'],
+    'header-end-period': [0, 'always'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
   },
   plugins: [
     {
