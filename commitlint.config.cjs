11ec7fdf10f228a7a172526dfffebc379c992f9a
Andrii Babushko <andriibabushko@gmail.com>
2024-06-21 15:50:27 +0300

    fix: correct linter issue №16

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,20 +6,5 @@ module.exports = {
     'header-end-period': [0, 'always'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-  },
-  plugins: [
-    {
-      rules: {
-        "header-case-start-capital": ({ raw }) => {
-          return [
-            /^[A-Z]/.test(raw),
-            "Commit message must start with a capital letter",
-          ];
-        },
-        "header-end-period": ({ header }) => {
-          return [/\.$/.test(header), "Commit message must end with a period"];
-        },
-      },
-    },
-  ],
+  }
 };
