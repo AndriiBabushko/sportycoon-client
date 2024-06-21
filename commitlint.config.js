6174bd07e26dc196e90eab60b9d5c2c3ea0e8a29
Andrii Babushko <andriibabushko@gmail.com>
2024-06-21 14:20:27 +0300

    Fix linter №11.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-min-length': [2, 'always', 20],
-    'header-case-start-capital': [2, 'always'],
-    'header-end-period': [2, 'always'],
+    "header-min-length": [2, "always", 10],
+    "header-case-start-capital": [2, "always"],
+    "header-end-period": [2, "always"],
   },
   plugins: [
     {
       rules: {
-        'header-case-start-capital': ({ raw }) => {
+        "header-case-start-capital": ({ raw }) => {
           return [
             /^[A-Z]/.test(raw),
-            'Commit message must start with a capital letter',
+            "Commit message must start with a capital letter",
           ];
         },
-        'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit message must end with a period'];
+        "header-end-period": ({ header }) => {
+          return [/\.$/.test(header), "Commit message must end with a period"];
         },
       },
     },
