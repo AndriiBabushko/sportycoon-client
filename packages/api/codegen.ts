aad3f1b273800b238efd73c61a4a770f516ca34c
Andrii Babushko <andriibabushko@gmail.com>
2024-09-02 10:40:46 +0300

    feat: setup e2e tests

--- packages/api/codegen.ts
+++ packages/api/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "https://sportycoon-api.up.railway.app/graphql",
+  schema: "https://sportycoon-server.onrender.com/graphql",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["gql/**/*.{ts,tsx}"],
   generates: {
