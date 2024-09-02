ac703353270f0b05e70b5cf1bf02a5a9ddc1adaa
Andrii Babushko <andriibabushko@gmail.com>
2024-09-02 12:15:24 +0300

    fix: change header-min-length in commitlint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-min-length': [2, 'always', 20],
+    'header-min-length': [2, 'always', 15],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
   },
