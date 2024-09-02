3c3ab0d2f32a2ca30d6a729eab2b230b9d12b007
Andrii Babushko <andriibabushko@gmail.com>
2024-09-02 10:40:46 +0300

    feat: setup e2e tests

--- packages/ui/lib/constants.ts
+++ packages/ui/lib/constants.ts
@@ -1,10 +1,10 @@
-// export enum ADMIN_PAGES {
-//   ROOT = "/",
-//   LOGIN = "/account/login",
-//   REGISTER = "/account/register",
-//   ACCOUNT = "/account",
-//   DASHBOARD = "/dashboard",
-// }
+export enum AdminPages {
+  ROOT = "/",
+  LOGIN = "/account/login",
+  REGISTER = "/account/register",
+  ACCOUNT = "/account",
+  DASHBOARD = "/dashboard",
+}
 
 // export enum CLIENT_PAGES {
 //   ROOT = "/",
